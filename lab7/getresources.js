d9f2590651946f560281891559f3e17b6f5f9ba3
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-12 15:30:58 +0100

    s

--- lab7/getresources.js
+++ lab7/getresources.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
           console.log(result);
           var output = "<table><thead><tr><th>Name</th><th>Provider</th><th>URL</th></thead><tbody>";
           for(var i in result){
-            output +="<tr><td>"+result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url+ ""<td></tr>"";
+            output+="<tr><td>" + result[i].name + "</td><td>" + result[i].provider + "</td><td>" + result[i].url + "</td></tr>";
           }
           output+="</tbody></table>";
           displayResources.html(output);
