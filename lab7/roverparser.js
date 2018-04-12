edb3f8116b2301f1e8ee70a660e62256f06d1a54
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-12 20:05:36 +0100

    dfb

--- lab7/roverparser.js
+++ lab7/roverparser.js
@@ -22,8 +22,9 @@
     + photos[i].camera.full_name + "</td><td>"
     + photos[i].img_src + "</td></td>"
     + "1" + "</td><td>"
-    alert(photos[i].rover.launch_date)
+
     + photos[i].rover.launch_date + "</td></tr>";
+      alert(photos[i].rover.launch_date);
   }
   output+="</tbody></table>";
 
