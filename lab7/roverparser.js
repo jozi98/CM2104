40917bfc831465079b20a10a52b3f1e8fd1b0c1a
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-12 19:42:29 +0100

    gth

--- lab7/roverparser.js
+++ lab7/roverparser.js
@@ -2,7 +2,7 @@
  $(document).ready(function () {
 
   $('#retrieve-data').click(function () {
-    alert("Hello");
+
   var displayResources = $('#display-data');
 
   displayResources.text('Loading Rover Data');
@@ -14,14 +14,14 @@
   {
   console.log(result.photos);
   var photos = result.photos //this data contains an array called photos
-  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th><th>PicDate</th><th>Launch Date</th></thead><tbody>";
+  var output="<table><thead><tr><th>Rover</th><th>Camera</th><th>Image URL</th><th>PicDate</th><th>LaunchDate</th></thead><tbody>";
 
   for (var i in photos)
   {
     output+="<tr><td>" + photos[i].rover.name + "</td><td>"
     + photos[i].camera.full_name + "</td><td>"
     + photos[i].img_src + "</td></td>"
-     + photos[i].earth_date + "</td><td>"
+    + photos[i].earth_date + "</td><td>"
     + photos[i].rover.launch_date + "</td></tr>";
   }
   output+="</tbody></table>";
