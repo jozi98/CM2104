b1889314c7d245ecb41942c1a72c94f70c759723
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-12 20:09:14 +0100

    egf

--- lab7/roverparser.js
+++ lab7/roverparser.js
@@ -20,11 +20,11 @@
   {
     output+="<tr><td>" + photos[i].rover.name + "</td><td>"
     + photos[i].camera.full_name + "</td><td>"
-    + photos[i].img_src + "</td></td>"
+    + photos[i].img_src + "</td><td>"
     + "1" + "</td><td>"
 
     + photos[i].rover.launch_date + "</td></tr>";
-      alert(photos[i].rover.launch_date);
+    
   }
   output+="</tbody></table>";
 
