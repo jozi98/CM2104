6eecf37ee75cdcd0164212d57e4da49ac4388ada
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-12 19:12:54 +0100

    dfd

--- lab7/roverparser.js
+++ lab7/roverparser.js
@@ -20,7 +20,7 @@
   {
     output+="<tr><td>" + photos[i].rover.name + "</td><td>"
     + photos[i].camera.full_name + "</td><td>";
-    + photos[i].img_src + "</td><td>";
+    + photos[i].img_src + "</td><tr>";
     // + photos[i].earth_date + "</td><td>";
     // + photos[i].rover.launch_date + "</td></tr>";
   }
