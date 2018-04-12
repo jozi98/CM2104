96742d6b915cb86031b35e77763bb01ef44bdd69
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-12 19:02:56 +0100

    df

--- lab7/roverparser.js
+++ lab7/roverparser.js
@@ -2,7 +2,7 @@
  $(document).ready(function () {
 
   $('#retrieve-data').click(function () {
-    
+    alert("Hello");
   var displayResources = $('#display-data');
 
   displayResources.text('Loading Rover Data');
