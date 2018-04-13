480e7b4cdf1363a82e6ef006a5651494f73f4fc2
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-13 11:27:14 +0100

    ss

--- lab7/roverparser.js
+++ lab7/roverparser.js
@@ -22,7 +22,7 @@
     alert(imgOut);
     output+="<tr><td>" + photos[i].rover.name + "</td><td>"
     + photos[i].camera.full_name + "</td><td>"
-    + '<img src = "'photos[i].img_src'">'   + "</td><td>"
+    // + '<img src = "'photos[i].img_src'">'   + "</td><td>"
     +  photos[i].earth_date  + "</td><td>"
     + photos[i].rover.launch_date + "</td></tr>";
 
