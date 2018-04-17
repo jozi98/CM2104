1cca4cbcb195d9111bdbae0727e82185e2518718
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-17 20:46:26 +0100

    ss

--- lab11/server.js
+++ lab11/server.js
@@ -11,6 +11,12 @@ var client = new Twitter({
 
 app.use(express.static('public'))
 app.get('/', function(req, res){
+  var params = {screen_name: 'nodejs'};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+   if (!error) {
+   console.log(tweets);
+   }
+  });
  res.send("Hello world! by express & twitter");
 });
 app.listen(8080);
