d35f29e19869b7490c24ca3c70afea2d4f965695
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-17 21:31:20 +0100

    xx

--- lab11/server.js
+++ lab11/server.js
@@ -13,8 +13,11 @@ app.use(express.static('public'))
 app.get('/', function(req, res){
   var params = {screen_name: 'nodejs'};
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-   if (!error) {
-     res.send(tweets);
+   if (error) {
+     res.send("erro occured when loading tweets");
+  }
+  else{
+    res.send(tweets);
   }
   });
  res.send("Hello world! by express & twitter");
