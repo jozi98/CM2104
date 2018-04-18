edf70c1ccc6302f1bef4f94732955ab386dcd3b5
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-18 23:19:03 +0100

    g

--- lab13-quotes/server.js
+++ lab13-quotes/server.js
@@ -39,56 +39,56 @@ app.get('/update', function(req,res) {
 
 
 
-app.get('/allquotes', function(req, res) {
-  db.collection('quotes').find().toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    var output = "<h1>All the quotes</h1>";
-    for (var i = 0; i < result.length; i++) {
-      output += "<div>"
-      output += "<h3>" + result[i].name + "</h3>"
-      output += "<p>" + result[i].quote + "</p>"
-      output += "</div>"
-    }
-    res.render('pages/index',{quotes:result});
-  });
-});
+// app.get('/allquotes', function(req, res) {
+//   db.collection('quotes').find().toArray(function(err, result) {
+//     if (err) throw err;
+//     console.log(result);
+//     var output = "<h1>All the quotes</h1>";
+//     for (var i = 0; i < result.length; i++) {
+//       output += "<div>"
+//       output += "<h3>" + result[i].name + "</h3>"
+//       output += "<p>" + result[i].quote + "</p>"
+//       output += "</div>"
+//     }
+//     res.render('pages/index',{quotes:result});
+//   });
+// });
 
-app.post('/add', function (req, res) {
-  db.collection('quotes').save(req.body, function(err, result) {
-    if (err) throw err;
-    console.log('saved to database')
-    res.redirect('/')
-  })
-})
-
-app.post('/filter', function(req, res) {
-  db.collection('quotes').find(req.body).toArray(function(err, result) {
-    if (err) throw err;
-
-    var output = "<h1>quotes by" +req.body.name+ "</h1>";
-
-    for (var i = 0; i < result.length; i++) {
-      output += "<div>"
-      output += "<h3>" + result[i].name + "</h3>"
-      output += "<p>" + result[i].quote + "</p>"
-      output += "</div>"
-    }
-  res.render('pages/filter',{quotes:result});  });
-});
-
-app.post('/delete', function(req, res) {
-  db.collection('quotes').deleteOne(req.body, function(err, result) {
-    if (err) throw err;
-    res.redirect('/');
-  });
-});
-
-app.post('/update', function(req, res) {
-  var query = { quote: req.body.quote };
-  var newvalues = { $set: {name: req.body.newname, quote: req.body.newquote } };
-  db.collection('quotes').updateOne(query,newvalues, function(err, result) {
-    if (err) throw err;
-    res.redirect('/');
-  });
-});
+// app.post('/add', function (req, res) {
+//   db.collection('quotes').save(req.body, function(err, result) {
+//     if (err) throw err;
+//     console.log('saved to database')
+//     res.redirect('/')
+//   })
+// })
+//
+// app.post('/filter', function(req, res) {
+//   db.collection('quotes').find(req.body).toArray(function(err, result) {
+//     if (err) throw err;
+//
+//     var output = "<h1>quotes by" +req.body.name+ "</h1>";
+//
+//     for (var i = 0; i < result.length; i++) {
+//       output += "<div>"
+//       output += "<h3>" + result[i].name + "</h3>"
+//       output += "<p>" + result[i].quote + "</p>"
+//       output += "</div>"
+//     }
+//   res.render('pages/filter',{quotes:result});  });
+// });
+//
+// app.post('/delete', function(req, res) {
+//   db.collection('quotes').deleteOne(req.body, function(err, result) {
+//     if (err) throw err;
+//     res.redirect('/');
+//   });
+// });
+//
+// app.post('/update', function(req, res) {
+//   var query = { quote: req.body.quote };
+//   var newvalues = { $set: {name: req.body.newname, quote: req.body.newquote } };
+//   db.collection('quotes').updateOne(query,newvalues, function(err, result) {
+//     if (err) throw err;
+//     res.redirect('/');
+//   });
+// });
