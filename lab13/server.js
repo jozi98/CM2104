f59a1b9835537226b77eeebde7c4a353544b852c
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-18 20:09:36 +0100

    ss

--- lab13/server.js
+++ lab13/server.js
@@ -1,14 +1,30 @@
 // server.js
 // load the things we need
 var express = require('express');
-var app = express();
+var app = express();13
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 // use res.render to load up an ejs view file
 // index page
+// app.get('/', function(req, res) {
+//  res.render('pages/index');
+// });
 app.get('/', function(req, res) {
- res.render('pages/index');
+ var drinks = [
+ { name: 'Bloody Mary', drunkness: 3 },
+ { name: 'Martini', drunkness: 5 },
+ { name: 'Scotch', drunkness: 10 }
+ ];
+ var tagline = "Any code of your own thatyou haven't looked at for six or more monthsmight as well have been written by someone else.";
+ res.render('pages/index', {
+ drinks: drinks,
+ tagline: tagline
+ });
 });
+
+
+
+
 // about page
 app.get('/about', function(req, res) {
  res.render('pages/about');
