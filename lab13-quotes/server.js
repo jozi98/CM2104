f37a1dea5f7a20643fbdf7f8816cd393dd46e2b5
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-18 23:47:02 +0100

    ff

--- lab13-quotes/server.js
+++ lab13-quotes/server.js
@@ -50,9 +50,10 @@ app.get('/allquotes', function(req, res) {
       output += "<p>" + result[i].quote + "</p>"
       output += "</div>"
     }
-    res.render('pages/index',{result:result;
+    res.render('pages/index',{result:result
   });
 });
+}
 
 // app.post('/add', function (req, res) {
 //   db.collection('quotes').save(req.body, function(err, result) {
