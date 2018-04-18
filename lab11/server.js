e8c53151a0fa6a2578fc2130a4653ef776fa2276
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-18 13:49:29 +0100

    yy

--- lab11/server.js
+++ lab11/server.js
@@ -10,22 +10,26 @@ var client = new Twitter({
 });
 
 app.use(express.static('public'))
-  app.get('/tweetsjson', function(req, res) {
-    var params = {
-      screen_name: 'nodejs'
-    };
-      client.get('statuses/user_timeline', params, function(error, tweets,response) {
+app.get('/getform', function(req, res){
 
-        if (!error) {
-          var json = [];
-          for (var i = 0; i < tweets.statuses.length; i++) {
-            json.push({
-              name: tweets.statuses[i].user.name,
-              text: tweets.statuses[i].text
-            });
-          }
-          res.send(JSON.stringify(json));
-        }
-      });
-    });
-    app.listen(8080);
+var name = req.query.name;
+
+  var params = {screen_name: name};
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
+   if (!error) {
+     var output = "";
+      for (var t = 0; t < tweets.length; t++) {
+      output += "<div>";
+      output += "<h2>" + tweets[t].user.screen_name + "<h2>";
+      output += "<p>" + tweets[t].text + "</p>"
+      output += "</div>";
+      }
+
+     res.send(output);
+    }
+
+  });
+ //res.send("Hello world! by express & twitter");
+
+});
+app.listen(8080);
