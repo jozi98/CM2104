186085a26a90f5e4945b977982c20fb3bb09f670
jozi99 <jozicricekt@hotmail.co.uk>
2018-04-19 11:52:21 +0100

    ff

--- lab13-quotes/server.js
+++ lab13-quotes/server.js
@@ -74,7 +74,7 @@ app.post('/filtered', function(req, res) {
       output += "<p>" + result[i].quote + "</p>"
       output += "</div>"
     }
-    res.render('pages/index',{quotes:result});
+    res.render('pages/filter',{quotes:result});
 });
 });
 //
